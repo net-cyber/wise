24c2fa20f588d9626ae1f011807c8c684ec7094a
net-cyber <ceo.natnael@gmail.com>
2024-11-01 01:57:36 +0300

    handeled the state of the drag using riverpod

--- lib/src/presentation/pages/initial/splash/splash_page.dart
+++ lib/src/presentation/pages/initial/splash/splash_page.dart
@@ -1,53 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:wise/src/core/constants/app_constants.dart';
 import 'package:wise/src/core/router/route_name.dart';
 import 'package:wise/src/presentation/theme/app_colors.dart';
+import 'riverpod_drag/drag_splash_provider.dart';
 
-class SplashPage extends StatefulWidget {
+class SplashPage extends ConsumerWidget {
   const SplashPage({super.key});
 
-  @override
-  State<SplashPage> createState() => _SplashPageState();
-}
-
-class _SplashPageState extends State<SplashPage> {
-  double _dragPosition = 0;
-
-  void _handleDragUpdate(DragUpdateDetails details) {
-    setState(() {
-      if (details.delta.dy < 0) {
-        _dragPosition += details.delta.dy;
-        _dragPosition = _dragPosition.clamp(-240, 0);
-      }
-    });
-
-    if (_dragPosition <= -200) {
+  void _handleDragUpdate(BuildContext context, WidgetRef ref, DragUpdateDetails details) {
+    ref.read(dragSplashProvider.notifier).updateDragPosition(details.delta.dy);
+    
+    if (ref.read(dragSplashProvider.notifier).shouldNavigate) {
       context.goNamed(RouteName.onboarding);
     }
   }
 
-  void _handleDragEnd(DragEndDetails details) {
-    if (_dragPosition > -200) {
-      setState(() {
-        _dragPosition = 0;
-      });
+  void _handleDragEnd(WidgetRef ref, DragEndDetails details) {
+    if (ref.read(dragSplashProvider) > -200) {
+      ref.read(dragSplashProvider.notifier).resetPosition();
     }
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final dragPosition = ref.watch(dragSplashProvider);
+
     return Scaffold(
       backgroundColor: AppColors.splashBackground,
       body: SafeArea(
-        
         bottom: false,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Spacer(),
-            // Logo
             Center(
               child: Image.asset(
                 AppConstants.wiseLogo,
@@ -57,15 +45,12 @@ class _SplashPageState extends State<SplashPage> {
               ),
             ),
             const Spacer(),
-            // Movable arrow with background image
             Stack(
               alignment: Alignment.bottomCenter,
               children: [
-                // Static background image container
                 ClipRRect(
                   borderRadius: BorderRadius.vertical(
                     top: Radius.circular(30.r),
-                    // bottom corners will remain square
                   ),
                   child: Container(
                     width: 70.w,
@@ -78,19 +63,22 @@ class _SplashPageState extends State<SplashPage> {
                     ),
                   ),
                 ),
-                // Movable arrow icon
                 GestureDetector(
-                  onVerticalDragUpdate: _handleDragUpdate,
-                  onVerticalDragEnd: _handleDragEnd,
+                  onVerticalDragUpdate: (details) => _handleDragUpdate(context, ref, details),
+                  onVerticalDragEnd: (details) => _handleDragEnd(ref, details),
                   child: Transform.translate(
-                    offset: Offset(0, _dragPosition),
+                    offset: Offset(0, dragPosition),
                     child: Container(
                       width: 60.w,
                       height: 60.h,
                       alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: AppColors.splashBackground.withOpacity(0.8), 
+                      ),
                       child: Icon(
                         Icons.arrow_upward,
-                        color: Colors.white,
+                        color: AppColors.white,
                         size: 30.sp,
                       ),
                     ),
