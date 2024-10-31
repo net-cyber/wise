7d96f519edef220c275864338807ed588b929ca0
net-cyber <ceo.natnael@gmail.com>
2024-10-31 19:33:07 +0300

    customize the appearance of the system UI overlays, for status bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:wise/src/core/constants/app_constants.dart';
 import 'package:wise/src/core/di/dependancy_manager.dart';
 import 'package:wise/src/core/utils/local_storage.dart';
+import 'package:wise/src/presentation/theme/app_colors.dart';
 
 void main() async {
   setUpDependencies();
   WidgetsFlutterBinding.ensureInitialized();
   await LocalStorage.getInstance();
-
+     SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: AppColors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+      statusBarBrightness: Brightness.dark,
+    ),
+  );
   runApp(const MyApp());
 }
 
