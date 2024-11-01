4a0153d83965f941d47e6a1ed83f47aa78eb6711
net-cyber <ceo.natnael@gmail.com>
2024-11-01 04:50:33 +0300

    pages page to export

---
+++ lib/src/presentation/pages/pages.dart
@@ -0,0 +1,25 @@
+export 'initial/splash/splash_page.dart';
+export 'initial/onboarding/onboarding_page.dart';
+export 'initial/splash/riverpod_drag/drag_splash_notifier.dart';
+export 'initial/splash/riverpod_drag/drag_splash_provider.dart';
+export 'initial/splash/splash_page.dart';
+
+export 'home/home_page.dart';
+export 'main/shell_page.dart';
+export 'main/card/card_page.dart';
+export 'main/card/widgets/action_buttons.dart';
+export 'main/card/widgets/apple_wallet_button.dart';
+export 'main/card/widgets/card_app_bar.dart';
+export 'main/card/widgets/card_carousel.dart';
+export 'main/card/widgets/card_number.dart';
+
+export 'main/home/widgets/balance_card.dart';
+export 'main/home/widgets/filter_chip_button.dart';
+export 'main/home/widgets/shimmer_container.dart';
+export 'main/home/widgets/top_bar.dart';
+export 'main/home/widgets/transaction_item.dart';
+
+export 'main/home/widgets/loading_widgets/balance_card_shimmer.dart';
+export 'main/home/widgets/loading_widgets/filter_chip_shimmer.dart';
+export 'main/home/widgets/loading_widgets/top_bar_shimmer.dart';
+export 'main/home/widgets/loading_widgets/transaction_shimmer.dart';
