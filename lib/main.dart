0181eb47445935a62e4dd803268477b1f6fd5f7d
net-cyber <devnatnaels@gmail.com>
2025-01-24 02:07:01 +0300

    Refactor code structure and remove unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,8 @@ import 'package:flutter/services.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:wise/src/app_widget.dart';
-import 'package:wise/src/core/constants/app_constants.dart';
 import 'package:wise/src/core/di/dependancy_manager.dart';
 import 'package:wise/src/core/utils/local_storage.dart';
-import 'package:wise/src/presentation/theme/app_colors.dart';
 
 void main() async {
   setUpDependencies();
--- lib/src/presentation/pages/initial/splash/splash_page.dart
+++ lib/src/presentation/pages/initial/splash/splash_page.dart
@@ -1,13 +1,7 @@
 
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 import 'package:wise/src/core/presentation/widgets/loading.dart';
-import 'package:wise/src/core/router/route_name.dart';
-import 'package:wise/src/core/utils/utils.dart';
-import 'package:wise/src/presentation/theme/app_colors.dart';
 import 'riverpod/provider/splash_provider.dart';
 
 class SplashPage extends ConsumerStatefulWidget {
@@ -22,7 +16,7 @@ class _SplashPageState extends ConsumerState<SplashPage> {
   void initState() {
     super.initState();
     Future.delayed(
-      Duration.zero,
+      const Duration(seconds: 2),
       () {
         ref.read(splashProvider.notifier).getUserStatus(
           context,
